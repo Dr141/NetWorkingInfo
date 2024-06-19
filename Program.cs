8623bffd9997c67bb1964ead19d7592227e78b12
Rhemerson.monteiro <rhemerson.monteiro@engegraph.com.br>
2024-06-19 11:16:31 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using NetWorkingInfo.Utilitarios;
+
+Console.WriteLine("Sistema para consultar os endereços IP´s ativos na rede do computador(IP V4)");
+InfoRede rede = new InfoRede();
+Console.WriteLine();
+Console.Write("Deseja pesquisar qual index: ");
+var resultado = Console.ReadLine();
+int index;
+if(int.TryParse(resultado, out index))
+{
+    rede.BuscarEndereco(index);    
+    Console.Write("Deseja imprimir o ips em uso? (S ou N): ");
+    resultado = Console.ReadLine();
+    if(resultado is not null)
+    {
+        switch (resultado.ToUpper())
+        {
+            case "N":
+                break;
+            default:
+                rede.ImprimirResultado();
+                break;
+        }
+    }    
+}
+Console.ReadLine();
---
+++ Utilitarios/InfoRede.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace NetWorkingInfo.Utilitarios
+{
+    public class InfoRede
+    {
+        private int _contador;
+        private IPHostEntry _hostEntry;
+        public List<string> RedeList { get; set; }
+        
+        public InfoRede()
+        {
+            _hostEntry = Dns.GetHostEntry(Environment.MachineName);
+            RedeList = new List<string>();
+            ImprimirListaIps();
+        }
+
+        public void ImprimirListaIps()
+        {
+            try
+            {
+                if (_hostEntry is not null && _hostEntry.AddressList is not null) 
+                {
+                    Console.WriteLine($"Nome host: {_hostEntry.HostName}");
+                    Console.WriteLine("Index");
+                    Console.WriteLine("*");
+                    foreach (var address in _hostEntry.AddressList) 
+                    {
+                        _contador++;
+                        Console.WriteLine("{0} -> IP: {1}; ", _contador, address.MapToIPv4().ToString());
+                    }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine($"Erro: {ex.Message}"); }
+        }
+
+        public void BuscarEndereco(int index)
+        {
+            if(index > 1) index--;
+            string[] partesDoEndereco = _hostEntry.AddressList[index].MapToIPv4().ToString().Split('.');
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            options.DontFragment = true;
+            string data = "teste";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            for(int i = 1; i <= 254; i++)
+            {
+                string address = string.Concat(partesDoEndereco[0], ".", partesDoEndereco[1], ".", partesDoEndereco[2],".", i);
+                PingReply reply = pingSender.Send(address, timeout, buffer, options);
+                Console.WriteLine("Ping Address: {0} - result: {1}", reply.Address.ToString(), Enum.GetName(reply.Status));
+                if (reply.Status == IPStatus.Success)
+                {
+                    RedeList.Add(address);
+                }
+            }            
+        }
+
+        public void ImprimirResultado()
+        {
+            Console.WriteLine("Endereços ips encontrados:");
+            foreach (string address in RedeList)
+            {
+                Console.WriteLine($"{address}");
+            }
+        }
+    }
+}
